85d96a0ae71949194a73bab05e650193396faa93
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-23 03:25:53 +0100

    Add data validation middleware

---
+++ app/middlewares/validateData.js
@@ -0,0 +1,19 @@
+const ValidationError = require("../utils/Errors/validationErrors");
+
+const validate = (schema) => async (req, res, next) => {
+  try {
+    req.body = await schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+    next();
+  } catch (error) {
+    const errors = error.inner.map((err) => ({
+      field: err.path,
+      message: err.message,
+    }));
+    next(new ValidationError(errors));
+  }
+};
+
+module.exports = { validate };
