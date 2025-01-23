3793b4fb79daa05ee036f401499f5803f57d95c3
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-23 19:24:19 +0100

    Add user model and database initialization logic

---
+++ app/models/user.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {}
+  }
+  User.init(
+    {
+      firstname: DataTypes.STRING,
+      lastname: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+      underscored: true,
+      paranoid: true,
+    }
+  );
+  return User;
+};
