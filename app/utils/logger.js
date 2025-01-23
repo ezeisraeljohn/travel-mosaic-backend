c13ac5506d9bdcb85cbb8a3e8be5cf3f2fae20d2
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-23 19:24:36 +0100

    Enhance logger to include metadata and service name from environment variables

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,14 +1,22 @@
 const winston = require("winston");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Create a new winston logger instance
 const logger = winston.createLogger({
   level: "info", // Minimum level to log
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} ${level}: ${message}`;
+    winston.format.printf(({ timestamp, level, message, ...meta }) => {
+      // Include meta in the log output, if it exists
+      return `${timestamp} ${level}: ${message} ${
+        Object.keys(meta).length ? JSON.stringify(meta) : ""
+      }`;
     })
   ),
+  defaultMeta: {
+    service: `${process.env.APP_NAME}`,
+  },
   transports: [
     // Log to the console
     new winston.transports.Console({
