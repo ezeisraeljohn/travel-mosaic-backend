85aae3c6f4d398927c01cc5f127c343a8a3a1986
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-23 19:24:55 +0100

    Refactor server logging to use centralized logger utility

--- app/server.js
+++ app/server.js
@@ -1,26 +1,28 @@
 const { createServer } = require("http");
 const app = require("./application");
 const dotenv = require("dotenv");
-
+const logger = require("../app/utils/logger");
 dotenv.config();
 const { PORT, HOST } = process.env;
 
 const server = createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+  //   console.log(`Server is running on http://${HOST}:${PORT}`);
+  logger.info(`Server is running on http://${HOST}:${PORT}`);
 });
 
 process.on("SIGINT", () => {
-  console.log("Shutting the server down...");
+  logger.info("Shutting the server down...");
   server.close(() => {
-    console.log("Server has been shut down.");
+    logger.info("Server has been shut down.");
     process.exit(0);
   });
 });
 
 //uncauth Error
 process.on("uncaughtException", (error) => {
-  console.error("Uncaught Exception:", error);
+  //   console.error("Uncaught Exception:", error);
+  logger.error("Uncaught Exception:", error);
   process.exit(1);
 });
