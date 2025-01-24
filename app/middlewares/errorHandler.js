57f9e741b54aa74194890e83f88ab557a2a7c838
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-24 21:47:19 +0100

    Enhance error handling middleware to log JWT errors and provide specific responses for authorization and token expiration issues

--- app/middlewares/errorHandler.js
+++ app/middlewares/errorHandler.js
@@ -1,16 +1,47 @@
 const logger = require("../utils/logger");
 const BaseError = require("../utils/Errors/baseError");
+const jwt = require("jsonwebtoken");
+const { stack } = require("sequelize/lib/utils");
+const { or } = require("sequelize");
 const errorHandler = (err, req, res, next) => {
   if (err instanceof BaseError) {
-    logger.error(`Error occurred: ${err.message}`, {
+    logger.error(`BaseError: ${err}`, {
       stack: err.stack,
-      details: err.details || null,
+      origin: err.origin,
+      field: err.field,
     });
     return res.status(err.status).json({
       success: false,
       error: err.name,
       message: err.message,
       details: err.details || null,
+    });
+  }
+
+  if (err instanceof jwt.JsonWebTokenError) {
+    logger.error(
+      `JWT >>> JsonWebTokenError: Permission denied, Unauthorized access`,
+      {
+        stack: err.stack,
+      }
+    );
+    return res.status(err.status).json({
+      success: false,
+      error: "Authorization Failed",
+      message: "Permission denied, Unauthorized access",
+      details: err?.details || null,
+    });
+  }
+
+  if (err instanceof jwt.TokenExpiredError) {
+    logger.error(`JWT >>> TokenExpiredError: Token Expired`, {
+      stack: err.stack,
+    });
+    return res.status(err.status).json({
+      success: false,
+      error: "Authentication Failed",
+      message: "Token Expired",
+      details: err?.details || null,
     });
   }
 
