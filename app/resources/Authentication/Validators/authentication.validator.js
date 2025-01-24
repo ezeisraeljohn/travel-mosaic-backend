32604665a2a29c5d915ab656e697ba3143314d70
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-24 21:50:11 +0100

    Add validation schema for user signup using Yup

---
+++ app/resources/Authentication/Validators/authentication.validator.js
@@ -0,0 +1,15 @@
+const yup = require("yup");
+const { response } = require("../../../utils/helpers");
+
+const signupSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Must be a valid Email")
+    .required("Email field is required"),
+  password: yup
+    .string()
+    .min(8, "Must be at least 8 characters long")
+    .required("Password is required"),
+});
+
+module.exports = { signupSchema };
