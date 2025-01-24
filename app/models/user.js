8be5bb678c5d5bdc366c4275395d9673c27ac5d8
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-24 21:48:32 +0100

    Refactor User model to improve field definitions and validation

--- app/models/user.js
+++ app/models/user.js
@@ -1,22 +1,45 @@
 const { Model, DataTypes } = require("sequelize");
 
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize) => {
   class User extends Model {
-    static associate(models) {}
+    static associate(models) {
+      // Define associations here if needed in the future
+    }
   }
   User.init(
     {
-      firstname: DataTypes.STRING,
-      lastname: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "first_name",
+        defaultValue: "", //Prevent null issues if left blank
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "last_name",
+        defaultValue: "", // Prevent null issues if left blank
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, // Ensure unique constraint is enforced
+        validate: {
+          isEmail: true, // Validate email format
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
     {
       sequelize,
       modelName: "User",
       tableName: "users",
-      underscored: true,
-      paranoid: true,
+      underscored: true, // Maps camelCase to snake_case in DB
+      paranoid: true, // Enables soft deletes using `deleted_at`
+      timestamps: true, // Enables `created_at` and `updated_at`
     }
   );
   return User;
