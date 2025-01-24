a44ba97d8c363408ca0cd0ab6faf6b28fad315e7
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-24 21:48:54 +0100

    Add signup controller to handle user registration

---
+++ app/resources/Authentication/controllers/authentication.controller.js
@@ -0,0 +1,24 @@
+const {
+  loginService,
+  signupService,
+} = require("../Services/authentication.service");
+const { response } = require("../../../utils/responses");
+
+/**
+ * @description - The Signup controller that registers the user into the system.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ *
+ * @returns {Object} - The response object.
+ */
+const signup = async (req, res, next) => {
+  try {
+    const result = await signupService(req, res, next);
+    return response(result)("Signup")(res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup };
