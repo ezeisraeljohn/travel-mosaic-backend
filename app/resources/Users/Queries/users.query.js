a4fbc6d85869a42d0a4bf46eb3a4718b9cd1b2e3
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-24 21:50:35 +0100

    Add getUserByEmailQuery function to retrieve user by email with error logging

---
+++ app/resources/Users/Queries/users.query.js
@@ -0,0 +1,17 @@
+const logger = require("../../../utils/logger");
+const { User } = require("../../../models");
+const user = require("../../../models/user");
+
+const getUserByEmailQuery = async (email) => {
+  try {
+    return User.findOne({ where: { email } });
+  } catch (error) {
+    logger.error("Error Finding User", {
+      stack: error.stack,
+    });
+  }
+};
+
+module.exports = {
+  getUserByEmailQuery,
+};
