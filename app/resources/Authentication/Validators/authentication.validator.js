86a2b0c47b6b6b7b1f8ad6fe7b45a843c57af664
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-25 02:03:02 +0100

    Add login validation schema for user authentication

--- app/resources/Authentication/Validators/authentication.validator.js
+++ app/resources/Authentication/Validators/authentication.validator.js
@@ -12,4 +12,15 @@ const signupSchema = yup.object().shape({
     .required("Password is required"),
 });
 
-module.exports = { signupSchema };
+const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Must be a valid Email")
+    .required("Email field is required"),
+  password: yup
+    .string()
+    .min(8, "Must be at least 8 characters long")
+    .required("Password is required"),
+});
+
+module.exports = { signupSchema, loginSchema };
