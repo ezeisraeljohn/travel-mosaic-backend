dc91e641096f07c57017fde4f7a4519da7a1e1d6
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-25 02:01:44 +0100

    Add login controller to handle user authentication

--- app/resources/Authentication/controllers/authentication.controller.js
+++ app/resources/Authentication/controllers/authentication.controller.js
@@ -3,7 +3,6 @@ const {
   signupService,
 } = require("../Services/authentication.service");
 const { response } = require("../../../utils/responses");
-
 /**
  * @description - The Signup controller that registers the user into the system.
  * @param {Object} req - The request object.
@@ -21,4 +20,20 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+/**
+ * @description - The Login controller that logs the user into the system.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {Object} - The response object.
+ */
+const login = async (req, res, next) => {
+  try {
+    const result = await loginService(req, res, next);
+    return response(result)("Login")(res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, login };
