ebed3c938383b8d91e406f6aa52b60605f2686a3
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-25 02:02:04 +0100

    Add login route with authentication middleware and validation

--- app/resources/Authentication/Routers/authentication.router.js
+++ app/resources/Authentication/Routers/authentication.router.js
@@ -1,9 +1,31 @@
 const express = require("express");
-const { signup } = require("../controllers/authentication.controller");
-const { signupSchema } = require("../Validators/authentication.validator");
+const { signup, login } = require("../controllers/authentication.controller");
+const AuthenticactionError = require("../../../utils/Errors/authenticationError");
+const {
+  signupSchema,
+  loginSchema,
+} = require("../Validators/authentication.validator");
 const { validate } = require("../../../middlewares/validateData");
+const passport = require("passport");
 
 const router = express.Router();
 router.post("/signup", validate(signupSchema), signup);
+router.post(
+  "/login",
+  validate(loginSchema),
+  (req, res, next) => {
+    passport.authenticate("local", { session: false }, (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        next(new AuthenticactionError("Invalid email or password", 401));
+      }
+      req.user = user;
+      next(); // Pass control to the next middleware (login controller)
+    })(req, res, next); // Invoke the middleware with req, res, and next
+  },
+  login
+);
 
 module.exports = router;
