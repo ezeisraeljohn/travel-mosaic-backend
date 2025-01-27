25d101693fa4a02a4b0bea80e5036bb429bfe98b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-27 12:03:55 +0100

    Add migration files for questionnaires, trips, itineraries, and itinerary activities tables

---
+++ app/migrations/20250127102535-add-trip-table.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("trips", {
+      id: {
+        type: Sequelize.UUID,
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        field: "user_id",
+      },
+      destination: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: "start_date",
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: "end_date",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: "updated_at",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("trips");
+  },
+};
