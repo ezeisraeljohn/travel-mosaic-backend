fd980f28c4111cafd3c724d86ff8b1cc5476a360
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-27 23:28:10 +0100

    Add migrations to make password field nullable and create federated credentials table

---
+++ app/migrations/20250127124405-add-federated_credentials_table.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("federated_credentials", {
+      id: {
+        type: Sequelize.UUID,
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        field: "user_id",
+      },
+      provider: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      providerId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: "provider_id",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: "updated_at",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
