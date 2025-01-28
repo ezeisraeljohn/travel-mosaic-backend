61a7f4419c742a93511f849ffc9c084fabc08693
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-28 11:14:46 +0100

    Remove console logs from data validation middleware for cleaner output

--- app/middlewares/validateData.js
+++ app/middlewares/validateData.js
@@ -3,12 +3,10 @@ const logger = require("../utils/logger");
 
 const validate = (schema) => async (req, res, next) => {
   try {
-    console.log("Validated data recieved the request");
     req.body = await schema.validate(req.body, {
       abortEarly: false,
       stripUnknown: true,
     });
-    console.log("Exiting the validated data");
     next();
   } catch (error) {
     logger.error(`Error Validating data`);
