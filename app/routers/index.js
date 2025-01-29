0f34fbad32aacac90e52dca542c5ade7b70924a6
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-29 12:04:16 +0100

    Add questionnaire router to main application routes

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,8 +1,10 @@
 const AuthenticationRouuter = require("../resources/Authentication/Routers/authentication.router");
+const QuestionnaireRouter = require("../resources/Questionnaires/Routers/questionnaire.router");
 const express = require("express");
 
 const router = (app) => {
   app.use("/api/v1", AuthenticationRouuter);
+  app.use("/api/v1", QuestionnaireRouter);
 };
 
 module.exports = router;
