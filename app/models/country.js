e2a0165b3dfa771c3c800a26261675476a3fdd20
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-02-03 23:38:05 +0100

    Add Country model with associations to City and UUID primary key

---
+++ app/models/country.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const uuid = require("uuid");
+
+module.exports = (sequelize) => {
+  class Country extends Model {
+    static associate(models) {
+      Country.hasMany(models.City, {
+        foreignKey: "countryId",
+        onDelete: "CASCADE",
+        as: "cities",
+      });
+    }
+  }
+  Country.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: () => uuid.v4(),
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Country",
+      tableName: "countries",
+      underscored: true,
+      timestamps: true,
+    }
+  );
+  return Country;
+};
