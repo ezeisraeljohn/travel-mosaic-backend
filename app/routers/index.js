784d104b82b743d479d1f3715d118c398c8942a6
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-02-05 01:17:12 +0100

    Fix typo in AuthenticationRouter import and add routes for Cities, Hotspots, and Trips

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,10 +1,15 @@
-const AuthenticationRouuter = require("../resources/Authentication/Routers/authentication.router");
+const AuthenticationRouter = require("../resources/Authentication/Routers/authentication.router");
 const QuestionnaireRouter = require("../resources/Questionnaires/Routers/questionnaire.router");
-const express = require("express");
+const CityRouter = require("../resources/Cities/Routers/cities.router");
+const HotspotsRouter = require("../resources/Hotspots/Routers/hotspots.router");
+const TripRouter = require("../resources/Trips/Routers/trips.router");
 
 const router = (app) => {
-  app.use("/api/v1", AuthenticationRouuter);
+  app.use("/api/v1", AuthenticationRouter);
   app.use("/api/v1", QuestionnaireRouter);
+  app.use("/api/v1", CityRouter);
+  app.use("/api/v1", HotspotsRouter);
+  app.use("/api/v1", TripRouter);
 };
 
 module.exports = router;
