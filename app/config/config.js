5ddbd4226f19a638c0b03c95d1cf1b68c6982344
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-02-06 21:05:09 +0100

    fix: Update production database configuration in config.js

--- app/config/config.js
+++ app/config/config.js
@@ -9,6 +9,12 @@ const {
   DB_DEV_TYPE,
   DB_DEV_PORT,
   DB_DEV_DATABASE,
+  DB_PROD_DATABASE,
+  DB_PROD_HOST,
+  DB_PROD_PASSWORD,
+  DB_PROD_USERNAME,
+  DB_PROD_TYPE,
+  DB_PROD_PORT,
 } = process.env;
 module.exports = {
   development: {
@@ -35,11 +41,11 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: DB_DEV_USERNAME,
-    password: DB_DEV_PASSWORD,
-    database: DB_DEV_DATABASE,
-    host: DB_DEV_HOST,
-    dialect: DB_DEV_TYPE,
+    username: DB_PROD_USERNAME,
+    password: DB_PROD_PASSWORD,
+    database: DB_PROD_DATABASE,
+    host: DB_PROD_HOST,
+    dialect: DB_PROD_TYPE,
     seederStorage: "json",
     seederStoragePath: "sequelizeSeedData.json",
     seederStorageTableName: "sequelize_data",
