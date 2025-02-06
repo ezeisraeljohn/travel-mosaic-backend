86a86bb9e56f01640fa953c2f417888a6801076e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-02-06 23:00:46 +0100

    feat: Add migration for itineraries table

---
+++ app/migrations/20250127102537-add-itinerary-table.js
@@ -0,0 +1,68 @@
+("use strict");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("itineraries", {
+      id: {
+        type: Sequelize.UUID,
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      tripId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "trips",
+          key: "id",
+        },
+        field: "trip_id",
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      hotspots: {
+        type: Sequelize.ARRAY(Sequelize.UUID),
+        allowNull: true,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: "updated_at",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("itineraries");
+  },
+};
